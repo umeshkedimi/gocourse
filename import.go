491a902ff2ab0d547391cb01c76b84587feb6cdc
umeshkedimi <umesh836@gmail.com>
2025-05-07 19:26:05 +0530

    Working with import packages and http response examples

--- import.go
+++ import.go
@@ -1,18 +1,21 @@
 package main
 
-import "fmt"
-import foo "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, Go Standard Library!")
-	fmt.Println("This is a simple HTTP server.")
-
-	resp, err := foo.Get("https://jsonplaceholder.typicode.com/posts/1")
+	// Example use of import packages and http response
+	fmt.Println("Importing packages in Go")
+	fmt.Println("Using net/http package to make a request")
+	resp, err := http.Get("https://JSONPlaceholder.typicode.com/posts/1")
 	if err != nil {
-		fmt.Println("Error: ", err)
+		fmt.Println("Error making request:", err)
 		return
 	}
 	defer resp.Body.Close()
-
-	fmt.Println("HTTP Response Status:", resp.Status)
+	fmt.Println("Response status:", resp.Status)
+	fmt.Println("Response headers:", resp.Header)
+	fmt.Println("Response body:", resp.Body)
 }
